14fe05a3d17c7f1b282dd622e39243faeb33b567
Vladyslav Konovalchuk <berlitio600@gmail.com>
2020-05-18 03:12:26 +0300

    Created custom Tooltip

---
+++ src/components/shared/TollTip/ToolTip.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ToolTip = ({styles, counter = 0, handleModalOpen = () => ({})}) => {
+    return (
+        <div
+            style={styles}
+        >
+            This word has {counter} synonyms.
+            <span onClick={handleModalOpen}>Show more...</span>
+        </div>
+    );
+};
+ToolTip.propTypes = {
+    styles: PropTypes.object.isRequired,
+    handleModalOpen: PropTypes.func,
+    counter: PropTypes.number,
+}
+export default ToolTip;
